7bcaaad4fb859da74ae4f65df3840dd6e741f392
yoonmi <xduathx@naver.com>
2022-06-06 09:56:29 +0900

    1일차

---
+++ momentum/app.js
@@ -0,0 +1,22 @@
+
+// #2.2 변수
+// #2.3 const(상수) : 값 재할당 불가능. - 항상쓰기
+// #2.3 let : 값 재할당 가능 - 가끔
+// #2.3 var : - 쓰지않기
+const a = 5;
+const b = 2;
+let myName = "yoom";
+
+console.log(a+b)
+console.log(a*b)
+console.log(a/b)
+console.log("hello " + myName)
+
+myName = "yoom mi"
+console.log("your name is " + myName)
+
+// #2.4 
+//undefined : 값이 들어가있지 않은 것.
+const amIFat = true;
+let something;
+console.log(something, amIFat)
