f4e90d5d07e5de52aa1d00ca7af73e3d1c8a38b3
yoonmi <xduathx@naver.com>
2022-06-13 22:37:30 +0900

    ## login

---
+++ momentum/login/app.js
@@ -0,0 +1,33 @@
+const loginForm = document.querySelector('#login-form');
+const loginInput = loginForm.querySelector('input');
+const greeting = document.querySelector('#greeting');
+
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
+
+function onLoginSubmit(e){
+    e.preventDefault();
+
+    loginForm.classList.add(HIDDEN_CLASSNAME)
+    const username = loginInput.value;
+
+    localStorage.setItem(USERNAME_KEY,username)
+    paintGreetings(username)
+}
+
+function paintGreetings(username){
+    greeting.innerText = `hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME)
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY)
+
+if(savedUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME)
+    loginForm.addEventListener('submit',onLoginSubmit)
+}else{
+    // show the greetings
+    console.log('ddd')
+    paintGreetings(savedUsername)
+}
