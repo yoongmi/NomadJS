dfb0306d510fe6c9bd90709c822c9ce82ae4f53e
yoonmi12 <xduathx@naver.com>
2022-06-15 09:37:05 +0900

    ## clock

---
+++ momentum/clock/clock.js
@@ -0,0 +1,13 @@
+
+const clock = document.querySelector("h1#clock")
+
+function getClock(){
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minute = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minute}:${seconds}`
+}
+getClock()
+
+setInterval(getClock,1000)
